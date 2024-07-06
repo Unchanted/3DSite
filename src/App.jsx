e481d2f5b1eb3fdcd8d4bcc68d45b1bbc8535d83
Unchanted <omkkdwivedi@gmail.com>
2024-07-06 18:31:24 +0530

    ground reflector

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,46 @@
 import * as THREE from 'three'
 import React, { Suspense, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Reflector, Text } from '@react-three/drei'
+import { Reflector, Text, useTexture, Stars } from '@react-three/drei'
 
-const StaticText = (props) => (
-  <Text 
-    font="/Inter-Bold.woff" 
-    fontSize={1} 
-    letterSpacing={-0.06} 
-    textAlign='center' 
-    {...props}
-  >
-    Big title
-    <meshBasicMaterial toneMapped={false} color="white" />
-  </Text>
-)
-
-const Ground = () => (
-  <Reflector 
-    blur={[400, 100]} 
-    resolution={512} 
-    args={[10, 10]} 
-    mirror={0.5} 
-    mixBlur={6} 
-    mixStrength={1.5} 
-    rotation={[-Math.PI / 2, 0, Math.PI / 2]}
-  >
-    {(Material, props) => (
-      <Material 
-        color="#a0a0a0" 
-        metalness={0.4}  
-        normalScale={[5, 5]} 
-        {...props} 
-      />
-    )}
-  </Reflector>
-)
+function StaticText(props){
+  return (
+    <>
+      <Text 
+      font="/Inter-Bold.woff" 
+      fontSize={1} 
+      letterSpacing={-0.06} 
+      textAlign='center' 
+      
+      {...props}
+      >
+      Big title
+      <meshBasicMaterial toneMapped={false} color="white" />
+      </Text>
+      <Text 
+        font="/Inter-Bold.woff" 
+        fontSize={1} 
+        letterSpacing={-0.06} 
+        textAlign='center' 
+        {...props}
+      >
+        Small title
+        <meshBasicMaterial toneMapped={false} color="white" />
+      </Text>
+    </>
+  )
+}
+function Ground() {
+  const [floor, normal] = useTexture(['/SurfaceImperfections003_1K_var1.jpg', '/SurfaceImperfections003_1K_Normal.jpg'])
+  return (
+    <Reflector blur={[400, 100]} resolution={512} args={[10, 10]} mirror={0.5} mixBlur={6} mixStrength={1.5} rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
+      {(Material, props) => <Material color="#a0a0a0" metalness={0.4} roughnessMap={floor} normalMap={normal} normalScale={[5, 5]} {...props} />}
+    </Reflector>
+  )
+}
 
 const Intro = () => {
   const [vec] = useState(() => new THREE.Vector3())
-  
   return useFrame((state) => {
     state.camera.position.lerp(
       vec.set(state.mouse.x * 5, 3 + state.mouse.y * 2, 14), 
@@ -57,6 +58,7 @@ const Scene = () => (
     </group>
     <spotLight position={[0, 10, 0]} intensity={0.5} />
     <directionalLight position={[10, 0, 1]} intensity={2} />
+    <Stars radius={100} depth={5} count={5000} factor={30} saturation={5} fade speed={1} />
     <Intro />
   </Suspense>
 )
